0ffb69d7d21df57a67bc746a49165c6a63cc481b
Samarth Shah <samarthshah3140@gmail.com>
2021-03-04 11:16:15 -0800

    Updates

---
+++ precache-manifest.e430f8f2dd649db977a94d92ff339699.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0a79f2e588a439ebe9b2305d62a056",
+    "url": "/EduApp/index.html"
+  },
+  {
+    "revision": "c5cd2cb05b2671672c22",
+    "url": "/EduApp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e5cc32fb4453ce156511",
+    "url": "/EduApp/static/css/main.9ff403b4.chunk.css"
+  },
+  {
+    "revision": "c5cd2cb05b2671672c22",
+    "url": "/EduApp/static/js/2.c3dd4855.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/EduApp/static/js/2.c3dd4855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5cc32fb4453ce156511",
+    "url": "/EduApp/static/js/main.f34e4517.chunk.js"
+  },
+  {
+    "revision": "f944fbc6073c380056c9",
+    "url": "/EduApp/static/js/runtime-main.c7c1ee21.js"
+  }
+]);
